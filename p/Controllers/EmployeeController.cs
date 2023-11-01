655a7c43527c6cb5e10e55d5e5482ad1d52116da
Iamneo <techadmin@iamneo.ai>
2023-11-01 10:55:13 +0000

    Backup Commit

--- p/Controllers/EmployeeController.cs
+++ p/Controllers/EmployeeController.cs
@@ -24,7 +24,7 @@ namespace p.Controllers
         // [Route("")]
         public IActionResult Index()
         {
-            var emp=db.Employees;
+            var emp=db.Employees.OrderBy(e=>e.EmployeeName);
             return View(emp);
         }
         [HttpGet]
