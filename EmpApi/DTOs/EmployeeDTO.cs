eb61f87ac80b2d4c48fbbaf689a1d7729b93f732
Iamneo <techadmin@iamneo.ai>
2023-11-04 09:14:47 +0000

    Backup Commit

---
+++ EmpApi/DTOs/EmployeeDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmpApi.Models;
+
+namespace EmpApi.DTOs
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeId{get;set;}
+        public string? EmployeeName{get;set;}
+        public double Salary{get;set;}
+
+        public string DepartmentName{get;set;}
+        // public int DesignationId{get;set;}
+
+        public virtual Department? Department{get;set;}
+    }
+}
--- EmpApi/Repository/EmployeeRepository.cs
+++ EmpApi/Repository/EmployeeRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmpApi.Models;
 
 namespace EmpApi.Repository
 {
@@ -9,6 +10,40 @@ namespace EmpApi.Repository
     {
         private EmpApiDbContext db;
 
-        public EmployeeRepository(EmpApiDbContext db)
+        public EmployeeRepository(EmpApiDbContext db){
+            this.db=db;
+        }
+
+        public Employee AddEmp(Employee e){
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return e;
+        }
+
+        public Employee FindEmp(int id){
+            var e=db.Employees.Find(id);
+            if(e!=null) return e;
+            return Nullable;
+        }
+
+        public Employee DeleteEmp(int id){
+            var e=db.Employees.Find(id);
+            if(e!=null){
+                db.Employees.Remove(e);
+                db.SaveChanges();
+                return e;
+            }
+            return null;
+        }
+
+        public Employee UpdateEmp(int id){
+            var e=db.Employees.Find(id);
+            if(e!=null){
+                db.Update(e);
+                db.SaveChanges();
+                return e;
+            }
+            return null;
+        }
     }
 }
