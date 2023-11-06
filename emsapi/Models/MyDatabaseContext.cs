8000cdc26177fa864995a70f5a27f8488886ab1a
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:47:06 +0000

    Backup Commit

---
+++ emsapi/Models/MyDatabaseContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace emsapi.Models;
+
+public partial class MyDatabaseContext : DbContext
+{
+    public MyDatabaseContext()
+    {
+    }
+
+    public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123;server=localhost;Database=MyDatabase;trusted_connection=false;persist security info=false;Encrypt=False;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__B2079BED9C1CD5CF");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04F11B2A511D7");
+
+            entity.Property(e => e.EmployeeName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.JoinDate).HasColumnType("datetime");
+            entity.Property(e => e.Salary).HasColumnType("money");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employees__Depar__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
