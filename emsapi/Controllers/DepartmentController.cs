549b6850f4342369ed773a40e2406955647c4861
Iamneo <techadmin@iamneo.ai>
2023-11-06 10:58:02 +0000

    Backup Commit

--- emsapi/Controllers/DepartmentController.cs
+++ emsapi/Controllers/DepartmentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using emsapi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,7 +12,19 @@ namespace emsapi.Controllers
     // [Route("[controller]")]
     public class DepartmentController : ControllerBase
     {
-        
+        IDepartment repo;
+        public DepartmentController(IDepartment _repo)
+        {
+            this.repo = _repo;
+        }
+ 
+        [HttpGet]
+        [Route("ListDept")]
+        public IActionResult GetDept()
+        {
+            var data = repo.GetDepartments();
+            return Ok(data);
+        }
         
     }
 }
