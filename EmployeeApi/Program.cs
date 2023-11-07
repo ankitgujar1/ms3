b024db37b2b4f0d332b4ba86c34fac24e87592c6
Iamneo <techadmin@iamneo.ai>
2023-11-07 06:26:13 +0000

    Backup Commit

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using EmployeeApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,24 @@ builder.Services.AddDbContext<ApiDbContext>(options=>{
     options.UseSqlServer(builder.Configuration.GetConnectionString("conStr"));
 });
 
-builder.Services
+IConfiguration con=builder.Configuration;
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(x=>{
+    x.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(token=>{
+    var key=Encoding.UTF8.GetBytes(con["JWT:Key"]);
+    token.TokenValidationParameters=new TokenValidationParameters{
+        ValidateIssuer=false,
+        ValidateAudience=false,
+        ValidateLifetime=true,
+        ValidateIssuerSigningKey=true,
+        ValidIssuers=con["JWT:Issuer"],
+        ValidAudience=con["JWT:Audience"],
+        IssuerSigningKey=new SymmetricSecurityKey(key)
+    };
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
