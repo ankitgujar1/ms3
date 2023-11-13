c02e73baeb01539bde5b5ff2a8b5d0c134d38f77
Iamneo <techadmin@iamneo.ai>
2023-11-13 11:21:06 +0000

    Backup Commit

--- wprac/Program.cs
+++ wprac/Program.cs
@@ -2,32 +2,45 @@ using Microsoft.EntityFrameworkCore;
 using wprac.Models;
 using wprac.Repository;
 var builder = WebApplication.CreateBuilder(args);
- 
+
 // Add services to the container.
- 
+
 builder.Services.AddControllers();
+builder.Services.AddCors(x =>
+{
+    x.AddPolicy("corsPolicy", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+
+        // policy.withOrigins();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>{
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("mystr"));
 });
-builder.Services.AddTransient<IEmployeeRepository , EmployeeRepository>();
- 
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+
 var app = builder.Build();
- 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
 app.UseHttpsRedirection();
- 
+
 app.UseAuthorization();
- 
+
+app.UseCors("corsPolicy");
+
 app.MapControllers();
- 
+
 app.Run();
- 
