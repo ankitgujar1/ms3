85c7655c9402c1e25262f79ead9feffbbca5165c
Iamneo <techadmin@iamneo.ai>
2023-11-23 19:16:03 +0000

    Backup Commit

---
+++ zMilestone3/angularapp/src/app/services/job.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JobApplication } from 'src/models/job-application.model';
+import { JobPosition } from 'src/models/job-position.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  constructor(private http:HttpClient) { }
+
+  getJobPostings():Observable<JobPosition[]>{
+    let header:HttpHeaders=new HttpHeaders({
+      Accept:"application/json"
+    })
+
+    return this.http.get<JobPosition[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/positions",{headers:header});
+  }
+
+  getJobApplications():Observable<JobApplication[]>{
+    let header:HttpHeaders=new HttpHeaders({
+      Accept:"application/json"
+    })
+
+    return this.http.get<JobApplication[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/applications",{headers:header});
+  }
+}
