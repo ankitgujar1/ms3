71a29ff71e693dfad929a544a648b54c700078e1
Iamneo <techadmin@iamneo.ai>
2023-11-24 04:10:41 +0000

    Backup Commit

--- zMilestone3/angularapp/src/app/services/job.service.ts
+++ zMilestone3/angularapp/src/app/services/job.service.ts
@@ -9,67 +9,68 @@ import { JobPosition } from 'src/models/job-position.model';
 })
 export class JobService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getJobPostings():Observable<JobPosition[]>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  getJobPostings(): Observable<JobPosition[]> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.get<JobPosition[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/positions",{headers:header});
+    return this.http.get<JobPosition[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/positions", { headers: header });
   }
 
-  getJobApplications():Observable<JobApplication[]>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  getJobApplications(): Observable<JobApplication[]> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.get<JobApplication[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/applications",{headers:header});
+    return this.http.get<JobApplication[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/applications", { headers: header });
   }
 
-  getPositionTitles():Observable<JobPosition[]>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  getPositionTitles(): Observable<JobPosition[]> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.get<JobPosition[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/position_title",{headers:header});
+    return this.http.get<JobPosition[]>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/position_title", { headers: header });
   }
 
-  createJopPosition(jobPositionData:JobPosition):Observable<JobPosition>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  createJopPosition(jobPositionData: JobPosition): Observable<JobPosition> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.post<JobPosition>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/position/add",jobPositionData,{headers:header});
+    return this.http.post<JobPosition>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/position/add", jobPositionData, { headers: header });
   }
 
-  applyForJob(jobApplicationData:JobApplication):Observable<JobApplication>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  applyForJob(jobApplicationData: JobApplication): Observable<JobApplication> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.post<JobApplication>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/application/add",jobApplicationData,{headers:header})
+    return this.http.post<JobApplication>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/application/add", jobApplicationData, { headers: header })
   }
 
-  updateApplicationStatus(applicationId:number,appName:string,newStatus:string):Observable<JobApplication>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  updateApplicationStatus(applicationId: number, appName: string, newStatus: string): Observable<JobApplication> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
-   let data:object={
-    Id:applicationId,
-    applicantName:appName,
-    status:newStatus
+
+    let data: object = {
+      id: applicationId,
+      applicantName: appName,
+      status: newStatus
     }
 
-    return this.http.put<JobApplication>(""+applicationId,data,{headers:header})
+    return this.http.put<JobApplication>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/position/update/" + applicationId, data, { headers: header })
   }
 
-  markJobAsClosed(jobId:number):Observable<JobPosition>{
-    let header:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  markJobAsClosed(jobId: number): Observable<JobPosition> {
+    let header: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    return this.http.delete<JobPosition>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/positions/delete"+jobId,{headers:header});
+    return this.http.delete<JobPosition>("https://8080-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Job/positions/delete/" + jobId, { headers: header });
   }
 
 
